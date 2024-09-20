1e6182b41a499856b20c77304dfe275810ace708
mahmutovAR <armax045@gmail.com>
2024-09-20 23:14:19 +0400

    new data 16

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 pytest_plugins = 'tests.fixtures'
 
 
 @pytest.fixture
 def browser():
-    # option = webdriver.ChromeOptions()
-    # option.add_argument("start-maximized")
-    # web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-    web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
     # web_driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
     web_driver.maximize_window()
     yield web_driver
     web_driver.quit()
