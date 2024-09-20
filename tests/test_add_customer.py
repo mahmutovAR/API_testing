e73164df7101d6a78a7db32e90cb781df1c4003f
mahmutovAR <armax045@gmail.com>
2024-09-20 22:29:39 +0400

    new data 13

--- tests/test_add_customer.py
+++ tests/test_add_customer.py
@@ -2,6 +2,7 @@ import allure
 from allure_commons.types import AttachmentType
 from pytest import fixture
 
+from pages import AddCustomerPage, CustomersPage
 
 
 @allure.feature("Banking Project")
@@ -31,27 +32,48 @@ from pytest import fixture
         - data in the Customers table corresponds to the entered""")
 def test_add_customer(browser: fixture, form_data: fixture, all_customers: fixture):
     with allure.step('Open "Banking Project" url'):
-        pass
+        add_customer_page = AddCustomerPage(browser)
+        add_customer_page.open_url()
 
     with allure.step('Open "Add Customer" form'):
-        pass
+        add_customer_page.go_to_form_page()
+
     with allure.step('Fill in "First Name" field'):
-        pass
+        add_customer_page.enter_first_name(form_data['first_name'])
+
     with allure.step('Fill in "Last Name" field'):
-        pass
+        add_customer_page.enter_last_name(form_data['last_name'])
+
     with allure.step('Fill in "Post Code" field'):
-        pass
+        add_customer_page.enter_post_code(form_data['post_code'])
+        allure.attach(browser.get_screenshot_as_png(),
+                      name="Add Customer page",
+                      attachment_type=AttachmentType.PNG)
 
     with allure.step('Submit data'):
-        pass
+        add_customer_page.submit_data()
+
     with allure.step('Close information window'):
-        pass
+        browser.switch_to.alert.accept()
+
     with allure.step('Open "Customers" tab'):
-        pass
+        customers_page = CustomersPage(browser)
+        customers_page.go_to_page()
 
     with allure.step('Find and check submitted data'):
-        pass
-        assert 2 == 1+1
+        customers_page.search_customer(form_data['first_name'])
+        allure.attach(browser.get_screenshot_as_png(),
+                      name="Customers page",
+                      attachment_type=AttachmentType.PNG)
+        assert get_customer_data(browser, all_customers) == form_data
 
     with allure.step('Delete submitted data'):
-        pass
+        customers_page.delete_customer()
+
+
+def get_customer_data(driver, get_all_customers: fixture) -> dict:
+    """Returns customer data."""
+    data = get_all_customers(driver).find('tr').find_all('td')
+    return {'first_name': data[0].text,
+            'last_name': data[1].text,
+            'post_code': data[2].text}
